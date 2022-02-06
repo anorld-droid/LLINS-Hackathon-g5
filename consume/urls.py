8fcee9dbf8e78fb658ec1722ed24f595573e40b3
Sheila Sharon <devsheilawambui@gmail.com>
2022-02-06 16:18:45 +0300

    edited templates

--- consume/urls.py
+++ consume/urls.py
@@ -5,8 +5,7 @@ urlpatterns=[
 
     path('dashboard',views.dashboard,name='dashboard'),
     path('kisumu',views.kisumu,name='kisumu'),
-
-
-
+    path('vihiga',views.vihiga,name='vihiga'),
+    path('busia',views.busia,name='busia'),
     
 ]
--- consume/views.py
+++ consume/views.py
@@ -6,74 +6,330 @@ import urllib,json
 
 # Create your views here.
 url='https://c630-41-89-192-24.ngrok.io/patients'
-
+url2='https://c630-41-89-192-24.ngrok.io/nets'
 
 def dashboard(request):
     res=urllib.request.urlopen(url)
-    datas=json.loads(res.read())
+    res2=urllib.request.urlopen(url2)
+    patientdata=json.loads(res.read())
+    netsdata=json.loads(res2.read())
+
 
     labels=[]
     years=[]
     months=[]
-    PatientsReceived=[]
-    PatientsNets=[]
-    PatientswithoutNets=[]
     counties=[]
 
-
-
-    for data in datas:
-        
-            labelling=data['County'] +'('+str(data['Month']) +')'+'('+str(data['Year']) +')'
-            labels.append(labelling)
-            years.append (data['Year'])
-            counties.append (data['County'])
-
-            months.append (data['Month'])
-            PatientsReceived.append (data['PatientsReceived'])
-            PatientsNets.append (data['PatientsNets'])
-            PatientswithoutNets.append (data['PatientswithoutNets'])
+    budgeted=0
+    issued=0
+    pending=0
 
 
 
-     
+    PatientsReceived2019=[]
+    PatientsNets2019=[]
+    PatientswithoutNets2019=[]
+
+    labels2=[]
+    years2=[]
+    months2=[]
+    counties2=[]
+    netsIssued=[]
+    netsProvided=[]
+
+    totalCounties=[]
+    totalPatientsReceived2019Kisumu=0
+    totalPatientsNets2019Kisumu=0
+    totalPatientswithoutNets2019Kisumu=0
+
+    totalPatientsReceived2019Busia=0
+    totalPatientsNets2019Busia=0
+    totalPatientswithoutNets2019Busia=0
+
+    totalPatientsReceived2019Vihiga=0
+    totalPatientsNets2019Vihiga=0
+    totalPatientswithoutNets2019Vihiga=0
+    
+
    
-    return render(request,'dashboard.html',{'datas':datas,'labels':labels,'years':years,'months':months,'counties':counties,'PatientsReceived':PatientsReceived,'PatientsNets':PatientsNets,'PatientswithoutNets':PatientswithoutNets})
+
+
+
+    for data in patientdata:
+        if data['Year'] == 2019:
+        
+                labelling=data['County'] +'('+str(data['Month']) +')'
+                labels.append(labelling)
+                years.append (data['Year'])
+                counties.append (data['County'])
+                months.append (data['Month'])
+                PatientsReceived2019.append (data['PatientsReceived'])
+                PatientsNets2019.append (data['PatientsNets'])
+                PatientswithoutNets2019.append (data['PatientswithoutNets'])
+                pending+=data['PatientswithoutNets']
+
+
+    for data in netsdata:
+        if data['Year'] == 2019:
+        
+            labelling=data['County'] +'('+str(data['Month']) +')'
+            labels2.append(labelling)
+
+            years2.append (data['Year'])
+            counties2.append (data['County'])
+            months2.append (data['Month'])
+
+            netsIssued.append(data['netsIssued'])
+            netsProvided.append(data['netsProvided'])
+            issued+=data['netsIssued']
+            budgeted+=data['netsProvided']
+
+         
+
+
+
+    return render(request,'dashboard.html',{'datas':patientdata,
+    'labels':labels,'years':years,'months':months,'counties':counties,
+    'PatientsReceived2019':PatientsReceived2019,'PatientsNets2019':PatientsNets2019,'PatientswithoutNets2019':PatientswithoutNets2019,
+    'datas2':netsdata,
+    'labels2':labels2,'years2':years2,'months2':months2,'counties2':counties2,
+    'netsIssued':netsIssued,'netsProvided':netsProvided,
+    'issued':issued,'budgeted':budgeted,'pending':pending
+    })
 
 def kisumu(request):
     res=urllib.request.urlopen(url)
-    datas=json.loads(res.read())
+    res2=urllib.request.urlopen(url2)
+    patientdata=json.loads(res.read())
+    netsdata=json.loads(res2.read())
+
 
     labels=[]
     years=[]
     months=[]
-    PatientsReceived=[]
-    PatientsNets=[]
-    PatientswithoutNets=[]
     counties=[]
 
+    PatientsReceived2019=[]
+    PatientsNets2019=[]
+    PatientswithoutNets2019=[]
 
+    labels2=[]
+    years2=[]
+    months2=[]
+    counties2=[]
+    netsIssued=[]
+    netsProvided=[]
 
-    for data in datas:
-        if data['County'] == 'kisumu':
+    
+    budgeted=0
+    issued=0
+    pending=0
+
+    kisumupatientsreceived = 0
+    # busiapatientswithoutnets
+    # busiapatientswithnets
+
+    for data in patientdata:
+        if data['Year'] == 2019:
+            if data['County'] == 'kisumu':
+
         
-            labelling=str(data['SubCounty']) +'('+str(data['Year']) +')'
-            labels.append(labelling)
-            years.append (data['Year'])
-            counties.append (data['County'])
+                labelling=data['County'] +'('+str(data['Month']) +')'
+                labels.append(labelling)
 
-            months.append (data['Month'])
-            PatientsReceived.append (data['PatientsReceived'])
-            PatientsNets.append (data['PatientsNets'])
-            PatientswithoutNets.append (data['PatientswithoutNets'])
+                years.append (data['Year'])
+                counties.append (data['County'])
 
-
-
-     
-   
-    return render(request,'kisumu.html',{'data':data,'labels':labels,'years':years,'months':months,'counties':counties,'PatientsReceived':PatientsReceived,'PatientsNets':PatientsNets,'PatientswithoutNets':PatientswithoutNets})
+                months.append (data['Month'])
+                PatientsReceived2019.append (data['PatientsReceived'])
+                PatientsNets2019.append (data['PatientsNets'])
+                PatientswithoutNets2019.append (data['PatientswithoutNets'])
+                pending+=data['PatientswithoutNets']
 
 
+    for data in netsdata:
+        if data['Year'] == 2019:
+            if data['County'] == 'kisumu':
+
+        
+                    labelling=data['County'] +'('+str(data['Month']) +')'
+                    labels2.append(labelling)
+
+                    years2.append (data['Year'])
+                    counties2.append (data['County'])
+                    months2.append (data['Month'])
+
+                    netsIssued.append(data['netsIssued'])
+                    netsProvided.append(data['netsProvided'])
+                    issued+=data['netsIssued']
+                    budgeted+=data['netsProvided']
+         
 
 
 
+    return render(request,'kisumu.html',{'datas':patientdata,
+    'labels':labels,'years':years,'months':months,'counties':counties,
+    'PatientsReceived2019':PatientsReceived2019,'PatientsNets2019':PatientsNets2019,'PatientswithoutNets2019':PatientswithoutNets2019,
+    'datas2':netsdata,
+    'labels2':labels2,'years2':years2,'months2':months2,'counties2':counties2,
+    'netsIssued':netsIssued,'netsProvided':netsProvided,
+    'issued':issued,'budgeted':budgeted,'pending':pending
+    })
+
+def vihiga(request):
+    res=urllib.request.urlopen(url)
+    res2=urllib.request.urlopen(url2)
+    patientdata=json.loads(res.read())
+    netsdata=json.loads(res2.read())
+
+
+    labels=[]
+    years=[]
+    months=[]
+    counties=[]
+
+    PatientsReceived2019=[]
+    PatientsNets2019=[]
+    PatientswithoutNets2019=[]
+
+    labels2=[]
+    years2=[]
+    months2=[]
+    counties2=[]
+    netsIssued=[]
+    netsProvided=[]
+
+    
+    budgeted=0
+    issued=0
+    pending=0
+
+
+
+    for data in patientdata:
+        if data['Year'] == 2019:
+            if data['County'] == 'vihiga':
+
+        
+                labelling=data['County'] +'('+str(data['Month']) +')'
+                labels.append(labelling)
+
+                years.append (data['Year'])
+                counties.append (data['County'])
+
+                months.append (data['Month'])
+                PatientsReceived2019.append (data['PatientsReceived'])
+                PatientsNets2019.append (data['PatientsNets'])
+                PatientswithoutNets2019.append (data['PatientswithoutNets'])
+                pending+=data['PatientswithoutNets']
+
+
+
+    for data in netsdata:
+        if data['Year'] == 2019:
+            if data['County'] == 'vihiga':
+
+        
+                    labelling=data['County'] +'('+str(data['Month']) +')'
+                    labels2.append(labelling)
+
+                    years2.append (data['Year'])
+                    counties2.append (data['County'])
+                    months2.append (data['Month'])
+
+                    netsIssued.append(data['netsIssued'])
+                    netsProvided.append(data['netsProvided'])
+                    issued+=data['netsIssued']
+                    budgeted+=data['netsProvided']
+         
+
+
+
+    return render(request,'vihiga.html',{'datas':patientdata,
+    'labels':labels,'years':years,'months':months,'counties':counties,
+    'PatientsReceived2019':PatientsReceived2019,'PatientsNets2019':PatientsNets2019,'PatientswithoutNets2019':PatientswithoutNets2019,
+    'datas2':netsdata,
+    'labels2':labels2,'years2':years2,'months2':months2,'counties2':counties2,
+    'netsIssued':netsIssued,'netsProvided':netsProvided,
+    'issued':issued,'budgeted':budgeted,'pending':pending
+
+    })
+
+def busia(request):
+    res=urllib.request.urlopen(url)
+    res2=urllib.request.urlopen(url2)
+    patientdata=json.loads(res.read())
+    netsdata=json.loads(res2.read())
+
+
+    labels=[]
+    years=[]
+    months=[]
+    counties=[]
+
+    PatientsReceived2019=[]
+    PatientsNets2019=[]
+    PatientswithoutNets2019=[]
+
+    labels2=[]
+    years2=[]
+    months2=[]
+    counties2=[]
+    netsIssued=[]
+    netsProvided=[]
+
+    
+    budgeted=0
+    issued=0
+    pending=0
+
+
+
+    for data in patientdata:
+        if data['Year'] == 2019:
+            if data['County'] == 'busia':
+
+        
+                labelling=data['County'] +'('+str(data['Month']) +')'
+                labels.append(labelling)
+
+                years.append (data['Year'])
+                counties.append (data['County'])
+
+                months.append (data['Month'])
+                PatientsReceived2019.append (data['PatientsReceived'])
+                PatientsNets2019.append (data['PatientsNets'])
+                PatientswithoutNets2019.append (data['PatientswithoutNets'])
+                pending+=data['PatientswithoutNets']
+
+
+
+    for data in netsdata:
+        if data['Year'] == 2019:
+            if data['County'] == 'busia':
+
+        
+                    labelling=data['County'] +'('+str(data['Month']) +')'
+                    labels2.append(labelling)
+
+                    years2.append (data['Year'])
+                    counties2.append (data['County'])
+                    months2.append (data['Month'])
+
+                    netsIssued.append(data['netsIssued'])
+                    netsProvided.append(data['netsProvided'])
+                    issued+=data['netsIssued']
+                    budgeted+=data['netsProvided']
+         
+
+
+
+    return render(request,'busia.html',{'datas':patientdata,
+    'labels':labels,'years':years,'months':months,'counties':counties,
+    'PatientsReceived2019':PatientsReceived2019,'PatientsNets2019':PatientsNets2019,'PatientswithoutNets2019':PatientswithoutNets2019,
+    'datas2':netsdata,
+    'labels2':labels2,'years2':years2,'months2':months2,'counties2':counties2,
+    'netsIssued':netsIssued,'netsProvided':netsProvided,
+    'issued':issued,'budgeted':budgeted,'pending':pending
+
+    })
