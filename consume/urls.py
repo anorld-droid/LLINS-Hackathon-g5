a9d8194bbf959fa75594441e4fd8a4fea9903b96
Charles Macharia <charlesmk2018@gmail.com>
2022-02-06 13:54:18 +0300

    Months lables Changed

--- consume/urls.py
+++ consume/urls.py
@@ -1,12 +1,7 @@
-from  django.urls import path
+from django.urls import path
 from .import views
 
-urlpatterns=[
-
-    path('dashboard',views.dashboard,name='dashboard'),
-    path('kisumu',views.kisumu,name='kisumu'),
-
-
-
-    
+urlpatterns = [
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('kisumu', views.kisumu, name='kisumu'),
 ]
